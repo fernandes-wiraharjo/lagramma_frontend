fb73335c6b1e27b9ef697a3a794737b576be5d6a
fernandes-wiraharjo <fernandeswiraharjo@gmail.com>
2025-05-28 17:42:20 +0700

    add model notif

---
+++ app/Models/Notification.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+
+class Notification extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'type',
+        'title',
+        'message',
+        'link',
+        'created_by',
+        'updated_by',
+        'created_at',
+        'updated_at'
+    ];
+
+    /**
+     * Relationships
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updater()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+}
