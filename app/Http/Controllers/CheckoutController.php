c0e7de6459f3b0dad02f339e7361b9999ee6c835
fernandes-wiraharjo <fernandeswiraharjo@gmail.com>
2025-10-31 16:56:24 +0700

    completion frontoffice payment confirmation

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Storage;
 use App\Models\Category;
 use App\Models\Product;
 use App\Models\ProductVariant;
