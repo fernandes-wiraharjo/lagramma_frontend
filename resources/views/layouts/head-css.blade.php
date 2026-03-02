d965fcd82f0c01394dde96f8226c13b0acea6355
Edy Tanto <edytanto.chai@gmail.com>
2026-03-02 21:13:33 +0700

    change font to Inter

--- resources/views/layouts/head-css.blade.php
+++ resources/views/layouts/head-css.blade.php
@@ -1,5 +1,9 @@
 @yield('css')
 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+
 <!-- Bootstrap Css -->
 <link href="{{ URL::asset('build/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
 <!-- Icons Css -->
